03d8d71835c0eaf71c95e4f9dbbd90635728d60e
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2024-11-12 10:32:00 +0100

    add tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import styles from "./page.module.css";
+import StatusLabel, { Status } from './components/StatusLabel';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div>
       <h1>Home Page</h1>
+      <StatusLabel status={Status.Active}>Active</StatusLabel>
+      <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+      <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+      <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
     </div>
   );
 }
