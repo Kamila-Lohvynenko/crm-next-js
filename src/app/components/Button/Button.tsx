83c53c2ea39b67ff229fab4cd4f0ef5b7a0dcebd
Kamila-Lohvynenko <kamila.lohvynenko@gmail.com>
2024-11-14 18:14:31 +0100

    add placeholders for fonts, styles for button

--- src/app/components/Button/Button.tsx
+++ src/app/components/Button/Button.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import styles from './Button.module.scss';
+import clsx from 'clsx';
 
-// export interface ButtonProps {}
+export interface ButtonProps {
+  children: string;
+  disabled?: boolean;
+}
 
-const Button = () => {
-  return <button className={styles.button}>Button</button>;
+const Button = ({ children, disabled, ...rest }: ButtonProps) => {
+  const buttonClassName = clsx(styles.button, disabled && styles.disabled);
+
+  return (
+    <button {...rest} className={buttonClassName}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
